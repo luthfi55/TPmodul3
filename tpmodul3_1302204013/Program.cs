1058ef6cddc493797d7a1400598d0c2baded6ec4
luthfi55 <luthfi12354@gmail.com>
2022-03-11 12:36:53 +0700

    first commit

---
+++ tpmodul3_1302204013/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace tpmodul3_1302204013
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
